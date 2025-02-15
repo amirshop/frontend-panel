48a461a95e6956f928d49df0f4f6a671691c20dc
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-15 17:44:58 +0330

    Update index.ts

--- src/menus/index.ts
+++ src/menus/index.ts
@@ -1,37 +1,42 @@
 export default [
   {
     key: 'TheDashboard',
-    label: 'dashboard',
+    label: 'Dashboard',
     icon: 'tabler:layout-dashboard',
   },
-//   {
-//     key: 'TheCompanyList',
-//     label: 'company',
-//     icon: 'ph:building-office',
-//   },
-//   {
-//     key: 'TheUsers',
-//     label: 'users',
-//     icon: 'tabler:users-group',
-//   },
-//   {
-//     key: 'TheServicesRequest',
-//     label: 'servicesRequest',
-//     icon: 'tabler:file-text',
-//   },
-//   {
-//     key: 'TheProjects',
-//     label: 'projects',
-//     icon: 'eos-icons:project-outlined',
-//   },
-//   {
-//     key: 'TheApiMarket',
-//     label: 'apiMarket',
-//     icon: 'ic:twotone-miscellaneous-services',
-//   },
-//   {
-//     key: 'ThePlans',
-//     label: 'plans',
-//     icon: 'iconoir:reports',
-//   },
+  {
+    key: 'TheSettings',
+    label: 'Settings',
+    icon: 'tabler:settings',
+  },
+  //   {
+  //     key: 'TheCompanyList',
+  //     label: 'company',
+  //     icon: 'ph:building-office',
+  //   },
+  //   {
+  //     key: 'TheUsers',
+  //     label: 'users',
+  //     icon: 'tabler:users-group',
+  //   },
+  //   {
+  //     key: 'TheServicesRequest',
+  //     label: 'servicesRequest',
+  //     icon: 'tabler:file-text',
+  //   },
+  //   {
+  //     key: 'TheProjects',
+  //     label: 'projects',
+  //     icon: 'eos-icons:project-outlined',
+  //   },
+  //   {
+  //     key: 'TheApiMarket',
+  //     label: 'apiMarket',
+  //     icon: 'ic:twotone-miscellaneous-services',
+  //   },
+  //   {
+  //     key: 'ThePlans',
+  //     label: 'plans',
+  //     icon: 'iconoir:reports',
+  //   },
 ]
