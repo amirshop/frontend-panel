f62d0cf8a27a688374741b465f5988f7ed9b1577
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-18 14:19:12 +0330

    Update PanelHeader.vue

--- src/core/layouts/PanelLayout/PanelHeader.vue
+++ src/core/layouts/PanelLayout/PanelHeader.vue
@@ -1,11 +1,7 @@
 <template>
   <LayoutHeader
-    class="!bg-gradient-to-r !leading-12 !p-inline-4 flex items-center"
-    :class="
-      configProviderStore.language === LanguagesEnum.ENGLISH
-        ? '!from-blue !to-primary'
-        : '!from-primary !to-blue'
-    "
+    class="!leading-12 !p-inline-4 flex items-center  !from-primary !to-primary-300"
+    :class="headerClass"
   >
     <div class="flex items-center ms-auto">
       <Dropdown :trigger="['click']" class="!ms-auto">
@@ -45,7 +41,7 @@
   </LayoutHeader>
 </template>
 <script lang="ts" setup>
-import { Dropdown, LayoutHeader, Menu, MenuItem, Divider } from 'ant-design-vue/es'
+import { Dropdown, LayoutHeader, Menu, MenuItem, Divider,Tooltip } from 'ant-design-vue/es'
 import { Icon } from '@iconify/vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/core/stores/auth.store'
@@ -53,8 +49,9 @@ import { useConfigProviderStore } from '@/core/stores/configProvider.store'
 import LockScreen from '@/core/components/LockScreen.vue'
 import { AzButton } from '@/core/components'
 import { useI18n } from 'vue-i18n'
-import { LanguagesEnum } from '@/core/enums'
+import { DirectionsEnum, LanguagesEnum } from '@/core/enums'
 import AzFullScreen from '@/core/components/AzFullScreen.vue'
+import { computed } from 'vue'
 
 const configProviderStore = useConfigProviderStore()
 const authStore = useAuthStore()
@@ -65,4 +62,12 @@ const logout = () => {
   authStore.logout()
   router.push({ name: 'TheAuthLogin' })
 }
+
+const headerClass = computed(() => {
+  if (configProviderStore.direction === DirectionsEnum.RTL) {
+    return '!bg-gradient-to-l'
+  } else {
+    return '!bg-gradient-to-r '
+  }
+})
 </script>
