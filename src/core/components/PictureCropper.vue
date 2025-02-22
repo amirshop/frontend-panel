f580e3747bb8399c5a22f894f3314aa284b50b99
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-22 12:26:16 +0330

    feat: add vue-advanced-cropper

--- src/core/components/PictureCropper.vue
+++ src/core/components/PictureCropper.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
-import { Modal } from 'ant-design-vue'
-import { reactive, ref } from 'vue'
-import { Input } from 'ant-design-vue/es'
-import VuePictureCropper, { cropper } from 'vue-picture-cropper'
+import { Modal, Image, Form, FormItem, Input } from 'ant-design-vue/es'
+import { ref, watch, type Ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { Cropper } from 'vue-advanced-cropper'
+import 'vue-advanced-cropper/dist/style.css'
+import AzButton from './AzButton.vue'
 
-const visible = ref(false)
 const isShowModal = ref<boolean>(false)
-const uploadInput = ref<HTMLInputElement | null>(null)
 const pic = ref<string>('')
-const result = reactive({
-  dataURL: '',
-  blobURL: '',
-})
+const cropperRef = ref()
+const { t } = useI18n()
+
+const width = ref<number>(300)
+const height = ref<number>(300)
+const aspectX = ref<number>(1)
+const aspectY = ref<number>(1)
 
 const handleFileChange = (event: Event) => {
   const file = (event.target as HTMLInputElement).files?.[0]
@@ -19,56 +22,108 @@ const handleFileChange = (event: Event) => {
     const reader = new FileReader()
     reader.onload = (e) => {
       pic.value = e.target?.result as string
-      isShowModal.value = true // Show the modal
+      isShowModal.value = true
     }
     reader.readAsDataURL(file)
   }
 }
 
-const ready = (e: any) => {
-  console.log('🚀 ~ ready ~ e:', e)
+const cropImage = () => {
+  if (cropperRef.value) {
+    const { canvas } = cropperRef.value?.getResult()
+    if (canvas) {
+      pic.value = canvas.toDataURL('image/png')
+      isShowModal.value = false
+    }
+  }
 }
 
-async function getResult() {
-  if (!cropper) return
-  const base64 = cropper.getDataURL()
-  const blob: Blob | null = await cropper.getBlob()
-  if (!blob) return
-
-  const file = await cropper.getFile({
-    fileName: pic.value,
-  })
-
-  console.log({ base64, blob, file })
-  result.dataURL = base64
-  result.blobURL = URL.createObjectURL(blob)
-  isShowModal.value = false
+watch([width, height, aspectX, aspectY], () => {
+  if (cropperRef.value) {
+    cropperRef.value?.refresh()
+  }
+})
+const remove = () => {
+  cropperRef.value = null
+  pic.value = ''
 }
 </script>
 
 <template>
-  <Input type="file" accept="image/*" @change="handleFileChange" />
+  <div class="relative w-40 h-40 image-input-container border-2 border-dashed border-primary">
+    <div class="" @click="$refs.fileInput.click()">
+      <input type="file" accept="image/*" ref="fileInput" @change="handleFileChange" hidden />
+      <Image v-if="pic" :src="pic" class="image-preview" />
+      <div v-else class="image-placeholder">{{ t('selectImage') }}</div>
+    </div>
+    <AzButton
+      type="primary"
+      danger
+      shape="circle"
+      size="small"
+      icon="tabler:x"
+      class="absolute top-2 end-2"
+      @click="remove"
+    />
+  </div>
   <Modal
     v-model:visible="isShowModal"
-    title="Crop Image"
+    :title="t('imageCropper')"
+    :ok-text="t('crop')"
     :ok-button-props="{
-      onClick: getResult,
+      onClick: cropImage,
+    }"
+    :cancel-button-props="{
+      onClick: () => (isShowModal = false),
     }"
   >
-    <VuePictureCropper
-      :boxStyle="{
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#f8f8f8',
-        margin: 'auto',
-      }"
-      :img="pic"
-      :options="{
-        viewMode: 1,
-        dragMode: 'crop',
-        aspectRatio: 16 / 9,
-      }"
-      @ready="ready"
-    />
+    <div class="cropper-container">
+      <Cropper ref="cropperRef" :src="pic" :stencil-props="{ aspectRatio: aspectX / aspectY }" />
+    </div>
+
+    <Form layout="vertical" class="grid grid-cols-2 gap-x-4">
+      <FormItem label="Width">
+        <Input v-model:value="width" type="number" />
+      </FormItem>
+      <FormItem label="Height">
+        <Input v-model:value="height" type="number" />
+      </FormItem>
+      <FormItem label="Aspect X">
+        <Input v-model:value="aspectX" type="number" />
+      </FormItem>
+      <FormItem label="Aspect Y">
+        <Input v-model:value="aspectY" type="number" />
+      </FormItem>
+    </Form>
+
+    <!-- <template #footer>
+      <AzButton @click="cropImage">{{ t('crop') }}</AzButton>
+      <AzButton @click="isShowModal = false">{{ t('cancel') }}</AzButton>
+    </template> -->
   </Modal>
 </template>
+
+<style scoped>
+.image-input-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+  text-align: center;
+}
+.image-preview {
+  max-width: 100%;
+  height: auto;
+}
+.image-placeholder {
+  color: #aaa;
+}
+.cropper-container {
+  width: 100%;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
