f59450f601c0c537c4666245e9ea1ae2d117252f
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-22 18:06:08 +0330

    Create Index.vue

---
+++ src/views/files/Index.vue
@@ -0,0 +1,10 @@
+<template>
+  <Card>
+    <AgGridVue />
+  </Card>
+</template>
+<script setup lang="ts">
+import  AgGridVue  from '@/core/components/ag-grid/Index.vue'
+import { Card } from 'ant-design-vue'
+
+</script>
