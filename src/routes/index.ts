315772ad0699f0ef98095cf2a1f3b86247066eed
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-22 12:26:40 +0330

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -35,6 +35,22 @@ export default [
   {
     path: '/shop-settings',
     name: 'TheShopSettings',
-    component: () => import('@/views/shop-settings/Index.vue'),
+    children: [
+      {
+        path: '',
+        name: 'TheShopSettingsPublic',
+        component: () => import('@/views/shop-settings/Index.vue'),
+      },
+      {
+        path: 'footer',
+        name: 'TheShopSettingsFooter',
+        component: () => import('@/views/shop-settings/FooterSettings.vue'),
+      },
+      {
+        path: 'header',
+        name: 'TheShopSettingsHeader',
+        component: () => import('@/views/shop-settings/HeaderSettings.vue'),
+      },
+    ],
   },
 ] as RouteRecordRaw[]
