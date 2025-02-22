75a970761425a9f704a39e7bfa911f20aac34028
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-22 17:27:41 +0330

    Update PictureCropper.vue

--- src/core/components/PictureCropper.vue
+++ src/core/components/PictureCropper.vue
@@ -1,129 +1,185 @@
+<template>
+  <div>
+    <div>
+      <div
+        v-if="!croppedImage"
+        class="relative w-max h-max rounded overflow-hidden border-2 border-dashed border-primary"
+      >
+        <label
+          for="amir-uploader"
+          class="text-primary w-30 h-30 flex justify-center items-center cursor-pointer"
+        >
+          {{ t('selectImage') }}
+        </label>
+        <input
+          type="file"
+          id="amir-uploader"
+          @change="selectedImageHandler"
+          accept="image/*"
+          :hidden="true"
+        />
+      </div>
+      <div v-if="croppedImage" class="relative z-10 w-max rounded overflow-hidden shadow">
+        <Image :src="croppedImage" class="!max-w-40 !max-h-40 z-10" />
+        <AzButton
+          @click="remove"
+          icon="tabler:x"
+          type="primary"
+          danger
+          shape="circle"
+          size="small"
+          class="absolute top-2 end-2 z-20"
+        />
+      </div>
+    </div>
+
+    <Modal
+      v-model:open="cropperModal.isOpen.value"
+      ok-text="Crop"
+      :ok-button-props="{
+        onClick: cropImage,
+        title: 'Crop',
+      }"
+      :mask-closable="false"
+      :closable="false"
+      width="40rem"
+    >
+      <div class="flex flex-col items-center justify-center gap-4">
+        <Cropper
+          ref="cropperRef"
+          :src="selectedImage"
+          :stencil-props="stencilProps"
+          :stencil-size="stencilSize"
+          :default-size="defaultSize"
+          :min-width="cropMode === 'minMax' ? minWidth : undefined"
+          :min-height="cropMode === 'minMax' ? minHeight : undefined"
+          :max-width="cropMode === 'minMax' ? maxWidth : undefined"
+          :max-height="cropMode === 'minMax' ? maxHeight : undefined"
+          :rounded="50"
+        />
+
+        <Form layout="vertical">
+          <FormItem>
+            <RadioGroup v-model:value="cropMode" button-style="solid">
+              <RadioButton value="free">Free</RadioButton>
+              <RadioButton value="aspect">Aspect Ratio</RadioButton>
+              <RadioButton value="size">Width / Height</RadioButton>
+              <RadioButton value="minMax">Min Max / Width Height</RadioButton>
+            </RadioGroup>
+          </FormItem>
+
+          <div v-if="cropMode === 'aspect'" class="flex gap-x-4">
+            <FormItem :label="t('aspectX')">
+              <InputNumber v-model:value="aspectX" placeholder="X" :min="1" />
+            </FormItem>
+            <FormItem :label="t('aspectY')">
+              <InputNumber v-model:value="aspectY" placeholder="Y" :min="1" />
+            </FormItem>
+          </div>
+
+          <div v-if="cropMode === 'size'" class="flex gap-x-4">
+            <FormItem :label="t('cropWidth')">
+              <InputNumber v-model:value="cropWidth" placeholder="Width" :min="50" />
+            </FormItem>
+            <FormItem :label="t('cropHeight')">
+              <InputNumber v-model:value="cropHeight" placeholder="Height" :min="50" />
+            </FormItem>
+          </div>
+
+          <div v-if="cropMode === 'minMax'" class="flex gap-x-4 items-center">
+            <FormItem :label="t('minWidth')">
+              <InputNumber v-model:value="minWidth" placeholder="Min Width" :min="50" />
+            </FormItem>
+
+            <FormItem :label="t('maxWidth')">
+              <InputNumber v-model:value="maxWidth" placeholder="Max Width" :min="50" />
+            </FormItem>
+            <Divider type="vertical" />
+            <FormItem :label="t('minHeight')">
+              <InputNumber v-model:value="minHeight" placeholder="Min Height" :min="50" />
+            </FormItem>
+            <FormItem :label="t('maxHeight')">
+              <InputNumber v-model:value="maxHeight" placeholder="Max Height" :min="50" />
+            </FormItem>
+          </div>
+        </Form>
+      </div>
+    </Modal>
+  </div>
+</template>
 <script setup lang="ts">
-import { Modal, Image, Form, FormItem, Input } from 'ant-design-vue/es'
-import { ref, watch, type Ref } from 'vue'
+import { computed, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { Cropper } from 'vue-advanced-cropper'
+import { AzButton } from '@/core/components'
+import { useModal } from '@/core/composable'
 import 'vue-advanced-cropper/dist/style.css'
-import AzButton from './AzButton.vue'
+import {
+  Modal,
+  Image,
+  Form,
+  FormItem,
+  Divider,
+  RadioGroup,
+  RadioButton,
+  InputNumber,
+} from 'ant-design-vue/es'
 
-const isShowModal = ref<boolean>(false)
-const pic = ref<string>('')
-const cropperRef = ref()
 const { t } = useI18n()
 
-const width = ref<number>(300)
-const height = ref<number>(300)
-const aspectX = ref<number>(1)
-const aspectY = ref<number>(1)
+const cropperRef = ref<InstanceType<typeof Cropper> | null>(null)
 
-const handleFileChange = (event: Event) => {
+const cropMode = ref<'aspect' | 'size' | 'minMax' | 'free'>('free')
+const aspectX = ref(1)
+const aspectY = ref(1)
+const cropWidth = ref(100)
+const cropHeight = ref(100)
+const minWidth = ref(100)
+const minHeight = ref(100)
+const maxWidth = ref(200)
+const maxHeight = ref(200)
+const selectedImage = ref('')
+const croppedImage = ref('')
+
+const stencilProps = computed(() =>
+  cropMode.value === 'aspect' ? { aspectRatio: aspectX.value / aspectY.value } : {},
+)
+
+const stencilSize = computed(() =>
+  cropMode.value === 'size' ? { width: cropWidth.value, height: cropHeight.value } : null,
+)
+
+const defaultSize = computed(() =>
+  cropMode.value === 'size' ? { width: cropWidth.value, height: cropHeight.value } : null,
+)
+
+const cropperModal = useModal()
+
+const cropImage = () => {
+  if (cropperRef.value) {
+    const result = cropperRef.value.getResult()
+    if (result && result.canvas) {
+      croppedImage.value = result.canvas.toDataURL()
+    }
+    cropperModal.close()
+  }
+}
+
+const selectedImageHandler = (event: Event) => {
   const file = (event.target as HTMLInputElement).files?.[0]
   if (file) {
     const reader = new FileReader()
     reader.onload = (e) => {
-      pic.value = e.target?.result as string
-      isShowModal.value = true
+      selectedImage.value = e.target?.result as string
+      cropperModal.open()
     }
     reader.readAsDataURL(file)
   }
 }
 
-const cropImage = () => {
-  if (cropperRef.value) {
-    const { canvas } = cropperRef.value?.getResult()
-    if (canvas) {
-      pic.value = canvas.toDataURL('image/png')
-      isShowModal.value = false
-    }
-  }
-}
-
-watch([width, height, aspectX, aspectY], () => {
-  if (cropperRef.value) {
-    cropperRef.value?.refresh()
-  }
-})
 const remove = () => {
   cropperRef.value = null
-  pic.value = ''
+  selectedImage.value = ''
+  croppedImage.value = ''
 }
 </script>
-
-<template>
-  <div class="relative w-40 h-40 image-input-container border-2 border-dashed border-primary">
-    <div class="" @click="$refs.fileInput.click()">
-      <input type="file" accept="image/*" ref="fileInput" @change="handleFileChange" hidden />
-      <Image v-if="pic" :src="pic" class="image-preview" />
-      <div v-else class="image-placeholder">{{ t('selectImage') }}</div>
-    </div>
-    <AzButton
-      type="primary"
-      danger
-      shape="circle"
-      size="small"
-      icon="tabler:x"
-      class="absolute top-2 end-2"
-      @click="remove"
-    />
-  </div>
-  <Modal
-    v-model:visible="isShowModal"
-    :title="t('imageCropper')"
-    :ok-text="t('crop')"
-    :ok-button-props="{
-      onClick: cropImage,
-    }"
-    :cancel-button-props="{
-      onClick: () => (isShowModal = false),
-    }"
-  >
-    <div class="cropper-container">
-      <Cropper ref="cropperRef" :src="pic" :stencil-props="{ aspectRatio: aspectX / aspectY }" />
-    </div>
-
-    <Form layout="vertical" class="grid grid-cols-2 gap-x-4">
-      <FormItem label="Width">
-        <Input v-model:value="width" type="number" />
-      </FormItem>
-      <FormItem label="Height">
-        <Input v-model:value="height" type="number" />
-      </FormItem>
-      <FormItem label="Aspect X">
-        <Input v-model:value="aspectX" type="number" />
-      </FormItem>
-      <FormItem label="Aspect Y">
-        <Input v-model:value="aspectY" type="number" />
-      </FormItem>
-    </Form>
-
-    <!-- <template #footer>
-      <AzButton @click="cropImage">{{ t('crop') }}</AzButton>
-      <AzButton @click="isShowModal = false">{{ t('cancel') }}</AzButton>
-    </template> -->
-  </Modal>
-</template>
-
-<style scoped>
-.image-input-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  cursor: pointer;
-  text-align: center;
-}
-.image-preview {
-  max-width: 100%;
-  height: auto;
-}
-.image-placeholder {
-  color: #aaa;
-}
-.cropper-container {
-  width: 100%;
-  height: 300px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
