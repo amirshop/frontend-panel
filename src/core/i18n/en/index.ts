71d0045f277d909a3da7b5d796edc240603b8d63
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-22 21:29:11 +0330

    Update index.ts

--- src/core/i18n/en/index.ts
+++ src/core/i18n/en/index.ts
@@ -11,6 +11,8 @@ export default {
   endDate: 'End date',
   file: 'file',
   files: 'Files',
+  media: 'Media',
+  mediaList: 'Media',
   filter: 'Filter',
   fullScreen: 'Full screen',
   language: 'language',
