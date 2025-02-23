1ec16185d40428ae89ba310654e69bce2acffc55
Amir Maghami <47292174+amirrr1987@users.noreply.github.com>
2025-02-23 18:43:10 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,29 +6,45 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import UnoCSS from 'unocss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools(), UnoCSS(), VitePWA({ registerType: 'autoUpdate',
-injectRegister: 'auto',
 
-workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      }
- })],
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    UnoCSS(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        name: 'My PWA App',
+        short_name: 'PWA App',
+        description: 'An example of a PWA app',
+        theme_color: '#ffffff',
+        background_color: '#000000',
+        display: 'fullscreen', // اینجا حالت Fullscreen تنظیم شده است
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  // css: {
-  //   preprocessorOptions: {
-  //     less: {
-  //       modifyVars: {
-  //         'primary-color': 'var(--primary-color)',
-  //         'link-color': 'var(--primary-color)',
-  //       },
-  //       javascriptEnabled: true,
-  //     },
-  //   },
-  // },
 })
