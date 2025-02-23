b58b3bc53e8a4af47f474ebde46fb0116e0f0bae
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-23 16:21:53 +0330

    Update Index.vue

--- src/views/users/Index.vue
+++ src/views/users/Index.vue
@@ -20,32 +20,12 @@ const columnList = reactive<ColumnsType>([
     title: 'Gender',
     dataIndex: 'gender',
     key: 'gender',
-    filterSearch: true,
-    filters: [
-      {
-        text: 'female',
-        value: 'female',
-      },
-      {
-        text: 'male',
-        value: 'male',
-      },
-    ],
-    onFilter: (value, record) => record.gender === value,
-    sorter: (a: TableDataType, b: TableDataType) => {
-      console.log(a.gender, b)
-
-      return a.gender.length - b.gender.length
-    },
-    sortDirections: ['ascend'],
   },
   {
     title: 'Email',
     dataIndex: 'email',
     key: 'email',
     customFilterDropdown: true,
-
-    onFilter: (value, record) => record.email.includes(value),
   },
 
   {
