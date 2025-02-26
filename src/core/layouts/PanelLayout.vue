10dbd5060a1e6dc7ddf843f1a5e700a3ff0ff9f0
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-26 14:16:52 +0330

    Create PanelLayout.vue

---
+++ src/core/layouts/PanelLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout class="h-screen">
+    <PanelSidebar />
+    <Layout>
+      <PanelHeader />
+      <PanelMain />
+    </Layout>
+  </Layout>
+</template>
+<script lang="ts" setup>
+import PanelSidebar from './PanelLayout/PanelSidebar.vue'
+import PanelHeader from './PanelLayout/PanelHeader.vue'
+import PanelMain from './PanelLayout/PanelMain.vue'
+import { Layout } from 'ant-design-vue/es'
+import { ref } from 'vue'
+</script>
