1277933277cb0d3afc13dde849875623f7a2f76c
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-26 23:28:17 +0330

    Update WTable.vue

--- src/components/WTable.vue
+++ src/components/WTable.vue
@@ -1,24 +1,50 @@
 <template>
   <Card>
+    <template #extra>
+      <ExportData :data="props.dataList" />
+      <Divider type="vertical" />
+      <Tooltip :title="t('resetTable')">
+        <AzButton
+          type="link"
+          size="small"
+          @click="() => emits('restTable')"
+          icon="tabler:refresh"
+        />
+      </Tooltip>
+    </template>
+
     <Table
       :columns="props.columnList"
       :data-source="props.dataList"
       :pagination="props.pagination"
-      @change="(e) => emits('change', e)"
+      :loading="props.loading"
+      @change="(e) => emits('changeTable', e)"
     >
     </Table>
   </Card>
 </template>
 <script setup lang="ts">
-import { Table, Card } from 'ant-design-vue/es'
+import AzButton from '@/core/components/AzButton.vue'
+import ExportData from '@/core/components/ExportData.vue'
+import { Table, Card, Tooltip, Divider } from 'ant-design-vue/es'
 import type { ColumnsType, TablePaginationConfig } from 'ant-design-vue/es/table'
-type DataList = any[]
+import { useI18n } from 'vue-i18n'
 
+type DataList = unknown[]
 interface Props {
   columnList: ColumnsType
   dataList: DataList
+  loading: boolean
   pagination: false | TablePaginationConfig
 }
 const props = withDefaults(defineProps<Props>(), {})
-const emits = defineEmits(['change'])
+const emits = defineEmits(['changeTable', 'restTable'])
+
+const { t } = useI18n()
 </script>
+<style lang="less">
+.ant-card-extra {
+  display: flex;
+  gap: 0.4rem;
+}
+</style>
