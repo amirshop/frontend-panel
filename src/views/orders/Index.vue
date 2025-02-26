8e4a2d0d6b3f1c6ff7e1646880b94445f7cd5215
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-26 23:28:36 +0330

    Update Index.vue

--- src/views/orders/Index.vue
+++ src/views/orders/Index.vue
@@ -1,50 +1,42 @@
 <template>
-  <AGTable />
-  <!-- <WTable
-    :column-list="table.columnList.value"
+  <WTable
+    :column-list="table.columnList"
     :data-list="table.dataList.value"
     :pagination="table.pagination.value"
-    :title="table.title.value"
-    @change="table.onChange"
-  /> -->
+    :title="table.title"
+    :loading="table.loading.value"
+    @changeTable="table.onChange"
+    @restTable="table.fetchTable"
+    @fetchTable="table.fetchTable"
+  />
 </template>
 
 <script setup lang="ts">
-import AGTable from '@/components/AGTable.vue'
 import WTable from '@/components/WTable.vue'
 import { useTable } from '@/composables/useTable.composable'
 import { onMounted } from 'vue'
 
-const table = useTable()
-
-// مقداردهی به عنوان و لیست ستون‌ها
-table.title.value = 'salasm'
-table.columnList.value.push(
-  ...[
+const table = useTable({
+  apiUrl: 'https://fakestoreapi.com/products',
+  title: 'salam',
+  columnList: [
     {
-      title: 'Name',
-      dataIndex: 'name',
-      sorter: true,
-      width: '20%',
+      title: 'title',
+      dataIndex: 'title',
+      key: 'title',
     },
     {
-      title: 'Gender',
-      dataIndex: 'gender',
-      filters: [
-        { text: 'Male', value: 'male' },
-        { text: 'Female', value: 'female' },
-      ],
-      width: '20%',
+      title: 'description',
+      dataIndex: 'description',
+      key: 'description',
     },
     {
-      title: 'Email',
-      dataIndex: 'email',
+      title: 'category',
+      dataIndex: 'category',
+      key: 'category',
     },
   ],
-)
+})
 
-// مقداردهی به apiUrl
-table.apiUrl.value = 'https://fakestoreapi.com/products'
-
-onMounted(async () => await table.runAsync)
+onMounted(async () => await table.fetchTable())
 </script>
