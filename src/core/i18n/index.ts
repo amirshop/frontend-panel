ade09306961ae59a3d8bcdf89a5d9c23e853f55d
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-26 10:24:05 +0330

    fix: update store

--- src/core/i18n/index.ts
+++ src/core/i18n/index.ts
@@ -1,13 +1,8 @@
 import { createI18n } from 'vue-i18n'
 import messages from './messages'
-<<<<<<< Updated upstream
-import { LanguagesEnum } from '@/enums'
-export default createI18n({
-=======
 import { LanguageEnum } from '@/core/enums'
 
 export const initI18n = createI18n({
->>>>>>> Stashed changes
   legacy: false,
   globalInjection: true,
   locale: LanguageEnum.ENGLISH,
