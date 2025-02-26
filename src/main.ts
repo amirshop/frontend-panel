0257e63ef80468631994af729cf5b05654713b6a
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-26 11:30:12 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
-import '@/assets/styles/main.less'
-import 'ant-design-vue/dist/reset.css'
-import 'virtual:uno.css'
+// import '@/assets/styles/main.less'
+// import 'ant-design-vue/dist/reset.css'
+// import 'virtual:uno.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './core/router'
-import i18n from './core/i18n'
-
+import { initI18n } from './core/i18n'
+import 'ant-design-vue/dist/reset.css'
+import 'virtual:uno.css'
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
-app.use(i18n)
+app.use(initI18n)
 
 app.mount('#app')
