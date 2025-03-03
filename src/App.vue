ea2ec84f985532867fd4ff81cdd357c35632e7db
Amir Maghami <Maghami.A1987@gmail.com>
2025-03-03 14:29:26 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,23 @@
 </template>
 <script setup lang="ts">
 import AppLayout from '@/core/layouts/AppLayout.vue'
-import { useHead } from '@vueuse/head'
+// import { useHead } from '@vueuse/head'
 import { useI18n } from 'vue-i18n'
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
 
-const { t } = useI18n()
+// const { t } = useI18n()
 
-const pageTitle = computed(() => t('siteTitle'))
-const metaDescription = computed(() => t('siteDescription'))
-const ogTitle = computed(() => t('dashboardTitle'))
+// const pageTitle = computed(() => t('siteTitle'))
+// const metaDescription = computed(() => t('siteDescription'))
+// const ogTitle = computed(() => t('dashboardTitle'))
 
-useHead({
-  title: pageTitle.value,
-  meta: [
-    { name: 'description', content: metaDescription.value },
-    { property: 'og:title', content: ogTitle.value },
-  ],
-})
+// onMounted(() =>
+//   useHead({
+//     title: pageTitle.value,
+//     meta: [
+//       { name: 'description', content: metaDescription.value },
+//       { property: 'og:title', content: ogTitle.value },
+//     ],
+//   }),
+// )
 </script>
